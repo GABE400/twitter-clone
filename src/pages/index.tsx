7b9e4c3180eeee20e75c852dca7712695f59422c
GABE400 <gabechipaya@gmail.com>
2023-05-15 23:05:04 +0200

    weekend change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ const Home: NextPage = () => {
   return (
     <>
       <header className="sticky top-0 z-10 border-b bg-white pt-2">
-        <h1 className="mb-2 px-4 text-lg font-bold">Home</h1>
+        <h1 className="mb-2 px-4 text-lg font-bold">Home Page</h1>
       </header>
       <NewTweetForm />
     </>
